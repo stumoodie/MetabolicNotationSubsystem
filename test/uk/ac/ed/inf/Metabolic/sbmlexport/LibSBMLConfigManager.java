112a1278f220392435ed5f515c295bf49da8f844
Richard Adams <richard.adams@ed.ac.uk>
2008-05-08 12:46:00 +0000

    load libSBML each test

--- test/uk/ac/ed/inf/Metabolic/sbmlexport/LibSBMLConfigManager.java
+++ test/uk/ac/ed/inf/Metabolic/sbmlexport/LibSBMLConfigManager.java
@@ -2,17 +2,18 @@ package uk.ac.ed.inf.Metabolic.sbmlexport;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Properties;
 
 public class LibSBMLConfigManager {
 	
 	private static boolean isWindows() {
 		return System.getProperty("os.name").indexOf("Win") != -1;
 	}
+	static int i;
+	static boolean isLoaded;
 
 	private static String getPath() throws Exception {
 		InputStream is = LibSBMLConfigManager.class.getResourceAsStream("PathToNativeSBMLLibraries.setUp");
@@ -31,7 +32,9 @@ public class LibSBMLConfigManager {
 		String path = getPath();
     	if(isWindows()) {
     	      System.load(path + "os/win32/sbmlj.dll");
+    	      markLoaded();
     	      return true;
+    		
     	} else {
     		return false;
     	}
@@ -39,5 +42,17 @@ public class LibSBMLConfigManager {
 			return false;
 		}
   
+	}
+	private static void markLoaded() throws IOException{
+		FileWriter fw = new FileWriter(new File ("./sbmlLoaded"));
+		fw.write("1");
+		fw.close();
+		
+	}
+	
+	private static boolean isLoaded() throws IOException{
+		File f = new File ("./sbmlLoaded");
+		return f.exists();
+		
 	}
 }
