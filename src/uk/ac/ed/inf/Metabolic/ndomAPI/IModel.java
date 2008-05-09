05eb09ec0e525c3d65be4e6c448885aa230b851e
Richard Adams <richard.adams@ed.ac.uk>
2008-05-09 08:00:16 +0000

    typos

--- src/uk/ac/ed/inf/Metabolic/ndomAPI/IModel.java
+++ src/uk/ac/ed/inf/Metabolic/ndomAPI/IModel.java
@@ -4,6 +4,6 @@ package uk.ac.ed.inf.Metabolic.ndomAPI;
 import java.util.List;
 
 public interface IModel extends IModelObject {
-  List<IComparment> getCompartmentList() ;
+  List<ICompartment> getCompartmentList() ;
   List<IReaction> getReactionList() ;
 }
