28e3e0614730ff3134956bde3751bf420ad16977
Anatoly Sorokin <lptolik@gmail.com>
2008-05-12 10:21:27 +0000

    Add  getters, based upon properties values

--- src/uk/ac/ed/inf/Metabolic/ndomAPI/IModel.java
+++ src/uk/ac/ed/inf/Metabolic/ndomAPI/IModel.java
@@ -6,4 +6,33 @@ public interface IModel extends IModelObject {
 	public List<ICompartment> getCompartmentList();
 
 	public List<IReaction> getReactionList();
+	
+	//Defined properties dictionaries
+	public List<String> getECnumberList();
+	
+	public List<String> getSmilesList();
+	
+	public List<String> getRoleList();
+	
+	public List<String> getCIDList();
+
+	public List<String> getChEBIList();
+
+	public List<String> getPubChemList();
+
+	public List<String> getInChIList();
+
+	public List<String> getGOTermList();
+
+/*
+//getters for object from property 
+ 	public List<String> get();
+
+	public List<String> get();
+
+	public List<String> get();
+
+	public List<String> get();
+
+*/
 }
