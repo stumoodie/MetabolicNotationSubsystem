9574be7a4fd2bb31bb77595cf6a42c7a717ef06d
Richard Adams <richard.adams@ed.ac.uk>
2008-05-18 23:11:13 +0000

    renamed more expressively

---
+++ src/uk/ac/ed/inf/Metabolic/sbmlexport/LibSBMLLoader.java
@@ -0,0 +1,63 @@
+package uk.ac.ed.inf.Metabolic.sbmlexport;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+ class LibSBMLLoader {
+	
+   protected static LibSBMLLoader instance;
+   
+   protected LibSBMLLoader () {
+	   
+   }
+   
+   public static  LibSBMLLoader getInstance () {
+	   if(instance == null){
+		   instance = new LibSBMLLoader();
+	   }
+	   return instance;
+   }
+   
+   public boolean loadLibrary () {
+	     String os = System.getProperty("os.name");
+	     if(os.contains("win") || os.contains("Win")){
+	    	 os="win32";
+	     }
+		 String lib = System.mapLibraryName("sbmlj");
+		 InputStream is = MetabolicSBMLExportAdapter.class.getResourceAsStream("/os/" +os +"/" + lib);
+		 File temp = new File(System.getProperty("java.io.tmpdir") + File.separator + lib);
+		
+		 try {
+			 temp.createNewFile();
+		 FileOutputStream fos = new FileOutputStream(temp);
+		 byte [] bytes = new byte[4096];
+		 int read = 0;
+		
+		 while( (read = is.read(bytes)) != -1){
+			fos.write(bytes, 0, read);
+		 }
+		 is.close();
+		 fos.close();
+		 }catch(IOException ie) {
+			 System.out.println(ie.getMessage());
+			 return false;
+		 }
+	
+		try {
+		  System.load(temp.getAbsolutePath());
+		}catch(UnsatisfiedLinkError e ) {
+			System.out.println(e.getMessage());
+			return false;
+		}finally{
+			if(temp.exists()){
+				temp.deleteOnExit();
+			}
+		}
+		return true;
+		
+	
+   }
+
+}
