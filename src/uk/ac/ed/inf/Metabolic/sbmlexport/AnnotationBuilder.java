dd627c36356ea191cac2aa7b795381d04c83eb0f
Richard Adams <richard.adams@ed.ac.uk>
2008-06-07 22:23:32 +0000

    *** empty log message ***

--- src/uk/ac/ed/inf/Metabolic/sbmlexport/AnnotationBuilder.java
+++ src/uk/ac/ed/inf/Metabolic/sbmlexport/AnnotationBuilder.java
@@ -2,6 +2,7 @@ package uk.ac.ed.inf.Metabolic.sbmlexport;
 
 import org.pathwayeditor.contextadapter.toolkit.ndom.IModelObject;
 
+// builds annotation
 public abstract class AnnotationBuilder {
 	protected IModelObject object;
 	String RDFst = "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:vCard=\"http://www.w3.org/2001/vcard-rdf/3.0#\" xmlns:bqbiol=\"http://biomodels.net/biology-qualifiers/\" xmlns:bqmodel=\"http://biomodels.net/model-qualifiers/\">";
