e9e407ecfd74ef6e54c1e728dec53c4fb6186196
Stuart Moodie <stuart.moodie@eightpillars.uk.com>
2008-06-17 14:37:06 +0000

    Added some review comments and made some revisions.

--- src/uk/ac/ed/inf/csb/Metabolic/excelexport/IExcelFileGenerator.java
+++ src/uk/ac/ed/inf/csb/Metabolic/excelexport/IExcelFileGenerator.java
@@ -3,54 +3,48 @@ package uk.ac.ed.inf.csb.Metabolic.excelexport;
 import java.io.File;
 import java.io.IOException;
 
-public interface IExcelFileGenerator {
+import uk.ac.ed.inf.Metabolic.ndomAPI.IModel;
+
+interface IExcelFileGenerator {
 	
 	/**
 	 * Performs a validation check on the loaded template. This is done to safeguard us for using a false or 
 	 * inappropriately loaded template file. 
 	 * @return true if the template is valid
 	 */
-	public boolean isTemplateValid () ;
+	boolean isTemplateValid () ;
 	
 	/**
-	 * Returns a String representation of the path of the TemplateFile. In the case that no such file exists, 
-	 * the value returned is null.
-	 * @return the path of the template file. 
+	 * Returns a String representation of the path of the TemplateFile. There will always be a template file.
+	 * @return the path of the template file, cannot be null. 
 	 */
-	public String getTemplateLocation () ;
+	String getTemplateLocation () ;
 	
 	/**
-	 * Creates a new workbook based on the template that is loaded and populates it with the data from the model.
-	 *In order for the method to be completed successfully, a check if the template is loaded properly is performed
-	 *as well as if the model is properly instantiated.
-	 * @throws IllegalStateException in the case that the validation of the template fails.
-	 * @throws NullPointerException in the case that the model is null.
+	 * Get the NDOM that is being written to the excel file.
+	 * @return The NDOM, cannot be null.
 	 */
-	public void createNewWorkbook () throws IllegalStateException, NullPointerException ;
+	IModel getMetabolicNDOM();
 	
 	/**
-	 * Responsible for the insertion of data from the model to the Model page of the template.
-	 * In order for this method to complete needs an already instantiated model and a valid workbook.
-	 * @throws IllegalStateException in the case that the validation of the template fails.
-	 * @throws NullPointerException in the case that the model is null.
+	 * Creates a new workbook based on the template provided. Requires that the template
+	 * file location is valid.
+	 * @throws IllegalStateException thrown if <code>isTemplateValid() == false</code>.
 	 */
-	public void populateModelPage () throws IllegalStateException, NullPointerException ;
+	void createNewWorkbook ();
 	
 	/**
-	 * Responsible for the insertion of data from the model to the Model page of the template.
-	 * In order for this method to complete needs an already instantiated model and a valid workbook.
-	 * @throws IllegalStateException in the case that the validation of the template fails.
-	 * @throws NullPointerException in the case that the model is null.
+	 * Tests if the workbook has been created.
+	 * @return true if it has, false otherwise.
 	 */
-	public void populateCompoundPage () throws IllegalStateException, NullPointerException ;
+	boolean wasWorkBookCreated();
 	
 	/**
-	 * Responsible for the insertion of data from the model to the Model page of the template.
-	 * In order for this method to complete needs an already instantiated model and a valid workbook.
-	 * @throws IllegalStateException in the case that the validation of the template fails.
-	 * @throws NullPointerException in the case that the model is null.
+	 * Populate the Model worksheet with **What Data? Nikos you should say what data is being written
+	 *  here**. The workbook must be created before this method id called. 
+	 * @throws IllegalStateException thrown if <code>wasWorkBookCreated() == false</code>.
 	 */
-	public void populateMacromoleculePage () throws IllegalStateException, NullPointerException ;
+	void populateModelPage ();
 	
 	/**
 	 * Responsible for the insertion of data from the model to the Model page of the template.
@@ -58,16 +52,35 @@ public interface IExcelFileGenerator {
 	 * @throws IllegalStateException in the case that the validation of the template fails.
 	 * @throws NullPointerException in the case that the model is null.
 	 */
-	public void populateReactionsPage () throws IllegalStateException, NullPointerException ;
+	void populateCompoundPage () throws IllegalStateException, NullPointerException ;
+	
+	/**
+	 * Responsible for the insertion of data from the model to the Model page of the template.
+	 * In order for this method to complete needs an already instantiated model and a valid workbook.
+	 * @throws IllegalStateException in the case that the validation of the template fails.
+	 * @throws NullPointerException in the case that the model is null.
+	 */
+	void populateMacromoleculePage () throws IllegalStateException, NullPointerException ;
+	
+	/**
+	 * Responsible for the insertion of data from the model to the Model page of the template.
+	 * In order for this method to complete needs an already instantiated model and a valid workbook.
+	 * @throws IllegalStateException in the case that the validation of the template fails.
+	 * @throws NullPointerException in the case that the model is null.
+	 */
+	void populateReactionsPage () throws IllegalStateException, NullPointerException ;
 	
 	/**
 	 * Writes the workbook in the given file. The target cannot be null. 
-	 * @param target the file in which the workbook is going to be written in.
+ 	// ***** What happens if the target is null ***
+ 	 * @param target the file in which the workbook is going to be written in.
+	// ** No the workbook need not be populated so we don't need this exception **
 	 * @throws IllegalStateException is thrown in the case that the workbook is not populated.
+	// ***  What does invalid mean? ***
 	 * @throws IllegalArgumentException in the case the target is invalid.
-	 * @throws IOException  	
+	 * @throws IOException  	** DOCUMENT THIS EXCEPTION **
 	 */
-	public void saveWorkBook ( File target ) throws IllegalStateException, IllegalArgumentException,  IOException ;
+	void saveWorkBook ( File target ) throws IllegalStateException, IllegalArgumentException,  IOException ;
 	
 
 }
